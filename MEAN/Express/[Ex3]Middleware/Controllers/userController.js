f1d42174eccd45d3d8a6a6c5cb2f43b0d1011cab
Fernando Romero <f.romerocuellar@globant.com>
2019-02-06 11:52:07 -0600

    Se realiza adicion de comentario del archivo userController.js

--- MEAN/Express/[Ex3]Middleware/Controllers/userController.js
+++ MEAN/Express/[Ex3]Middleware/Controllers/userController.js
@@ -4,7 +4,7 @@ module.exports= {
 
 function GetUser(req, res){
     
-    //--Colocamos una res.locals
+    //--Colocamos una res.locals o parametros locales
     res.locals.userName = req.params.userName;
     console.log(res.locals.userName);
     res.send('hola ' +res.locals.userName);
